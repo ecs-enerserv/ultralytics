9f44cdd26b1c9395b5f610a2ea5f7b2a6975f578
UltralyticsAssistant <web@ultralytics.com>
2024-03-28 11:24:35 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -833,7 +833,7 @@ class Albumentations:
         try:
             import albumentations as A
 
-            #check_version(A.__version__, "1.0.3", hard=True)  # version requirement
+            # check_version(A.__version__, "1.0.3", hard=True)  # version requirement
 
             # Transforms
             T = [
@@ -846,9 +846,12 @@ class Albumentations:
                 A.Emboss(p=0.02, alpha=(0.02, 0.1), strength=(0.05, 0.2)),
                 A.MultiplicativeNoise(p=0.02, multiplier=(0.95, 1.05)),
                 A.Rotate(p=0.02, limit=5),
-                A.ChromaticAberration(primary_distortion_limit=(0.01, 0.05), 
-                                     secondary_distortion_limit=(0.01, 0.07),
-                                     mode='random', p=0.02),
+                A.ChromaticAberration(
+                    primary_distortion_limit=(0.01, 0.05),
+                    secondary_distortion_limit=(0.01, 0.07),
+                    mode="random",
+                    p=0.02,
+                ),
                 A.ImageCompression(quality_lower=75, p=0.0),
             ]
             self.transform = A.Compose(T, bbox_params=A.BboxParams(format="yolo", label_fields=["class_labels"]))
@@ -1144,7 +1147,7 @@ def classify_augmentations(
                 f'"augmix", "autoaugment" or None'
             )
 
-    #if not disable_color_jitter:
+    # if not disable_color_jitter:
     secondary_tfl += [T.ColorJitter(brightness=hsv_v, contrast=hsv_v, saturation=hsv_s, hue=hsv_h)]
 
     final_tfl = [
@@ -1156,34 +1159,37 @@ def classify_augmentations(
     import albumentations as A
 
     # Transforms
-    albumentations_transform = A.Compose([
-        A.ISONoise(p=0.1, intensity=(0.02, 0.1)),
-        A.ToGray(p=0.02),
-        A.CLAHE(p=0.1, clip_limit=0.5),
-        A.MotionBlur(p=0.1, blur_limit=3),
-        A.GaussianBlur(p=1, blur_limit=(3, 5)),
-        A.RandomToneCurve(p=0.1, scale=0.05),
-        A.Sharpen(p=0.1, alpha=(0.02, 0.1), lightness=(0.8, 1.0)),
-        A.Emboss(p=0.1, alpha=(0.02, 0.1), strength=(0.05, 0.2)),
-        A.MultiplicativeNoise(p=0.1, multiplier=(0.95, 1.05)),
-        A.Rotate(p=0.1, limit=10, border_mode=cv2.BORDER_CONSTANT),
-        A.ChromaticAberration(primary_distortion_limit=(0.01, 0.05), 
-                              secondary_distortion_limit=(0.01, 0.07),
-                              mode='random', p=0.1),
-        A.RGBShift(p=0.5, r_shift_limit=10, g_shift_limit=10, b_shift_limit=10),
-        A.RingingOvershoot(p=0.1, blur_limit=(5, 11)),
-        A.OpticalDistortion(p=0.1, distort_limit=0.1, shift_limit=0.1, border_mode=cv2.BORDER_CONSTANT),
-        A.GridDistortion(p=0.1, num_steps=5, distort_limit=0.3, border_mode=cv2.BORDER_CONSTANT),
-        A.Perspective(p=0.1, scale=(0.05, 0.1)),
-        A.PiecewiseAffine(p=0.1, scale=(0.01, 0.015)), 
-        A.ImageCompression(quality_lower=75, p=0.0),
-        ])
+    albumentations_transform = A.Compose(
+        [
+            A.ISONoise(p=0.1, intensity=(0.02, 0.1)),
+            A.ToGray(p=0.02),
+            A.CLAHE(p=0.1, clip_limit=0.5),
+            A.MotionBlur(p=0.1, blur_limit=3),
+            A.GaussianBlur(p=1, blur_limit=(3, 5)),
+            A.RandomToneCurve(p=0.1, scale=0.05),
+            A.Sharpen(p=0.1, alpha=(0.02, 0.1), lightness=(0.8, 1.0)),
+            A.Emboss(p=0.1, alpha=(0.02, 0.1), strength=(0.05, 0.2)),
+            A.MultiplicativeNoise(p=0.1, multiplier=(0.95, 1.05)),
+            A.Rotate(p=0.1, limit=10, border_mode=cv2.BORDER_CONSTANT),
+            A.ChromaticAberration(
+                primary_distortion_limit=(0.01, 0.05), secondary_distortion_limit=(0.01, 0.07), mode="random", p=0.1
+            ),
+            A.RGBShift(p=0.5, r_shift_limit=10, g_shift_limit=10, b_shift_limit=10),
+            A.RingingOvershoot(p=0.1, blur_limit=(5, 11)),
+            A.OpticalDistortion(p=0.1, distort_limit=0.1, shift_limit=0.1, border_mode=cv2.BORDER_CONSTANT),
+            A.GridDistortion(p=0.1, num_steps=5, distort_limit=0.3, border_mode=cv2.BORDER_CONSTANT),
+            A.Perspective(p=0.1, scale=(0.05, 0.1)),
+            A.PiecewiseAffine(p=0.1, scale=(0.01, 0.015)),
+            A.ImageCompression(quality_lower=75, p=0.0),
+        ]
+    )
 
-    rgb_check = lambda image: image.convert('RGB') if image.mode != 'RGB' else image
+    rgb_check = lambda image: image.convert("RGB") if image.mode != "RGB" else image
 
-    album_tfl = [T.Lambda(lambda image: albumentations_transform(image=np.asarray(rgb_check(image)))['image'])]
+    album_tfl = [T.Lambda(lambda image: albumentations_transform(image=np.asarray(rgb_check(image)))["image"])]
 
     return T.Compose(primary_tfl + secondary_tfl + album_tfl + final_tfl)
+
 
 # NOTE: keep this class for backward compatibility
 class ClassifyLetterBox:
